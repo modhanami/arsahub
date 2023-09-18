ea88cc3814abb4c7fa848bc310e909d43818bd27
Rooted <modhanami@gmail.com>
2023-09-18 14:33:12 +0700

    Init project

---
+++ backend/settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "backend"
+
+//pluginManagement {
+//    resolutionStrategy {
+//        eachPlugin {
+//            if (requested.id.id == "org.hibernate.orm") {
+//                useModule("org.hibernate:hibernate-gradle-plugin:${requested.version}")
+//            }
+//        }
+//    }
+//}
