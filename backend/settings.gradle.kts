1cb802a990de9da999efb9b7b15ac3125f9712ed
Rooted <modhanami@gmail.com>
2023-09-22 23:08:36 +0700

    Remove pluginManagement for Hibernate

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,11 +1,1 @@
 rootProject.name = "backend"
-
-//pluginManagement {
-//    resolutionStrategy {
-//        eachPlugin {
-//            if (requested.id.id == "org.hibernate.orm") {
-//                useModule("org.hibernate:hibernate-gradle-plugin:${requested.version}")
-//            }
-//        }
-//    }
-//}
