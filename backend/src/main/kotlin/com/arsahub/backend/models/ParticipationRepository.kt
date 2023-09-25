0f213ccd8e0f9aa0610f38958e9355317724a304
Tawan Muadmuenwai <modhanami@gmail.com>
2023-09-25 20:37:25 +0700

    Remove dangling ParticipationRepository.kt

--- backend/src/main/kotlin/com/arsahub/backend/models/ParticipationRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.arsahub.backend.models;
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ParticipationRepository : JpaRepository<Participation, Long> {
-    fun findByUser(user: User): List<Participation>
-    fun findByEvent(event: Event): List<Participation>
-}
