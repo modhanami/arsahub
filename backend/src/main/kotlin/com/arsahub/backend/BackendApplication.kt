65cc83dd5ba7b098a62d5c2fa503080f84f00d06
Tawan Muadmuenwai <modhanami@gmail.com>
2023-09-25 19:20:07 +0700

    Add base for point system

--- backend/src/main/kotlin/com/arsahub/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/arsahub/backend/BackendApplication.kt
@@ -1,9 +1,13 @@
 package com.arsahub.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling
 class BackendApplication
 
 fun main(args: Array<String>) {
--- backend/src/main/kotlin/com/arsahub/backend/services/EventServiceImpl.kt
+++ backend/src/main/kotlin/com/arsahub/backend/services/EventServiceImpl.kt
@@ -5,8 +5,8 @@ import com.arsahub.backend.dtos.EventResponse
 import com.arsahub.backend.dtos.EventUpdateRequest
 import com.arsahub.backend.models.Event
 import com.arsahub.backend.models.Participation
-import com.arsahub.backend.models.ParticipationRepository
 import com.arsahub.backend.repositories.EventRepository
+import com.arsahub.backend.repositories.ParticipationRepository
 import com.arsahub.backend.repositories.UserRepository
 import jakarta.persistence.EntityNotFoundException
 import org.springframework.data.repository.findByIdOrNull
@@ -14,7 +14,11 @@ import org.springframework.stereotype.Service
 import java.time.Instant
 
 @Service
-class EventServiceImpl(private val eventRepository: EventRepository, private val userRepository: UserRepository, private val participationRepository: ParticipationRepository) :
+class EventServiceImpl(
+    private val eventRepository: EventRepository,
+    private val userRepository: UserRepository,
+    private val participationRepository: ParticipationRepository
+) :
     EventService {
 
     override fun createEvent(eventCreateRequest: EventCreateRequest): EventResponse {
@@ -81,7 +85,6 @@ class EventServiceImpl(private val eventRepository: EventRepository, private val
             Participation(
                 user = existingUser,
                 event = existingEvent,
-                pointsEarned = 0,
                 completed = false,
                 completedAt = Instant.now()
             )
