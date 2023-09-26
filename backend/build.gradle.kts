f4f0486dedceb7ddc82aa25c6281eb0372a2f088
Tawan Muadmuenwai <modhanami@gmail.com>
2023-09-26 10:14:34 +0700

    Update H2 version

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("com.h2database:h2:2.1.214")
+    implementation("com.h2database:h2")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
