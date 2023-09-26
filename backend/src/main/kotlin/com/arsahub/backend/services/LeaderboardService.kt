fca149ceaba248bdb0813965988a594fb2e27b56
Tawan Muadmuenwai <modhanami@gmail.com>
2023-09-26 21:36:17 +0700

    Move default service implementations to the same files as their interface

--- backend/src/main/kotlin/com/arsahub/backend/services/LeaderboardService.kt
+++ backend/src/main/kotlin/com/arsahub/backend/services/LeaderboardService.kt
@@ -1,7 +1,18 @@
 package com.arsahub.backend.services
 
 import com.arsahub.backend.dtos.LeaderboardResponse
+import org.springframework.stereotype.Service
 
 interface LeaderboardService {
     fun getTotalPoints(): LeaderboardResponse
+}
+
+@Service
+class LeaderboardServiceImpl : LeaderboardService {
+    override fun getTotalPoints(): LeaderboardResponse {
+        return LeaderboardResponse(
+            leaderboard = "total-points",
+            entries = listOf()
+        )
+    }
 }
