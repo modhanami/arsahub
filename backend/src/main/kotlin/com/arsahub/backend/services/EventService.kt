a8895ce5419628ff463c995e4ab280de62138862
Tawan Muadmuenwai <modhanami@gmail.com>
2023-09-26 21:36:17 +0700

    Move default service implementations to the same files as their interface

--- backend/src/main/kotlin/com/arsahub/backend/services/EventService.kt
+++ backend/src/main/kotlin/com/arsahub/backend/services/EventService.kt
@@ -4,6 +4,14 @@ import com.arsahub.backend.dtos.EventCreateRequest
 import com.arsahub.backend.dtos.EventResponse
 import com.arsahub.backend.dtos.EventUpdateRequest
 import com.arsahub.backend.models.Event
+import com.arsahub.backend.models.Participation
+import com.arsahub.backend.repositories.EventRepository
+import com.arsahub.backend.repositories.ParticipationRepository
+import com.arsahub.backend.repositories.UserRepository
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import java.time.Instant
 
 interface EventService {
     fun createEvent(eventCreateRequest: EventCreateRequest): EventResponse
@@ -16,3 +24,93 @@ interface EventService {
     fun listJoinedEvents(userId: Long): List<Event>
 }
 
+@Service
+class EventServiceImpl(
+    private val eventRepository: EventRepository,
+    private val userRepository: UserRepository,
+    private val participationRepository: ParticipationRepository
+) :
+    EventService {
+
+    override fun createEvent(eventCreateRequest: EventCreateRequest): EventResponse {
+        val savedEvent = eventRepository.save(eventCreateRequest.toEntity())
+        return EventResponse.fromEntity(savedEvent)
+    }
+
+    override fun updateEvent(eventId: Long, eventUpdateRequest: EventUpdateRequest): EventResponse {
+        val existingEvent = eventRepository.findByIdOrNull(eventId)
+            ?: throw EntityNotFoundException("Event with ID $eventId not found")
+
+        val updatedEvent = updateEvent(existingEvent, eventUpdateRequest)
+
+        val savedEvent = eventRepository.save(updatedEvent)
+        return EventResponse.fromEntity(savedEvent)
+    }
+
+    fun updateEvent(existingEvent: Event, eventUpdateRequest: EventUpdateRequest): Event {
+        if (eventUpdateRequest.title != null) {
+            existingEvent.title = eventUpdateRequest.title
+        }
+        if (eventUpdateRequest.description != null) {
+            existingEvent.description = eventUpdateRequest.description
+        }
+        if (eventUpdateRequest.location != null) {
+            existingEvent.location = eventUpdateRequest.location
+        }
+        if (eventUpdateRequest.startTime != null) {
+            existingEvent.startTime = eventUpdateRequest.startTime
+        }
+        if (eventUpdateRequest.endTime != null) {
+            existingEvent.endTime = eventUpdateRequest.endTime
+        }
+        return existingEvent
+    }
+
+    override fun getEvent(eventId: Long): Event? {
+        return eventRepository.findByIdOrNull(eventId)
+    }
+
+    override fun listEvents(): List<Event> {
+        return eventRepository.findAll()
+    }
+
+    override fun deleteEvent(eventId: Long) {
+        if (!eventRepository.existsById(eventId)) {
+            throw EntityNotFoundException("Event with ID $eventId not found")
+        }
+        eventRepository.deleteById(eventId)
+    }
+
+    override fun joinEvent(eventId: Long, userId: Long): Event {
+        val existingEvent = eventRepository.findByIdOrNull(eventId)
+            ?: throw EntityNotFoundException("Event with ID $eventId not found")
+
+        val existingUser = userRepository.findByIdOrNull(userId)
+            ?: throw EntityNotFoundException("User with ID $userId not found")
+
+        if (existingEvent.participations.any { it.user == existingUser }) {
+            throw IllegalArgumentException("User with ID $userId already joined event with ID $eventId")
+        }
+
+        val participation = participationRepository.save(
+            Participation(
+                user = existingUser,
+                event = existingEvent,
+                completed = false,
+                completedAt = Instant.now()
+            )
+        )
+
+        existingEvent.participations.add(participation)
+
+        return eventRepository.save(existingEvent)
+    }
+
+    override fun listJoinedEvents(userId: Long): List<Event> {
+        val existingUser = userRepository.findByIdOrNull(userId)
+            ?: throw EntityNotFoundException("User with ID $userId not found")
+
+        return existingUser.participations.map { it.event }
+
+    }
+}
--- backend/src/main/kotlin/com/arsahub/backend/services/OrganizerService.kt
+++ backend/src/main/kotlin/com/arsahub/backend/services/OrganizerService.kt
@@ -1,6 +1,9 @@
 package com.arsahub.backend.services
 
 import com.arsahub.backend.models.Organizer
+import com.arsahub.backend.repositories.OrganizerRepository
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.stereotype.Service
 
 interface OrganizerService {
     fun createOrganizer(organizer: Organizer): Organizer
@@ -8,4 +11,36 @@ interface OrganizerService {
     fun getOrganizer(organizerId: Long): Organizer?
     fun listOrganizers(): List<Organizer>
     fun deleteOrganizer(organizerId: Long)
+}
+
+@Service
+class OrganizerServiceImpl(private val organizerRepository: OrganizerRepository) : OrganizerService {
+
+    override fun createOrganizer(organizer: Organizer): Organizer {
+        return organizerRepository.save(organizer)
+    }
+
+    override fun updateOrganizer(organizerId: Long, updatedOrganizer: Organizer): Organizer {
+        val existingOrganizer = organizerRepository.findById(organizerId)
+            .orElseThrow { EntityNotFoundException("Organizer with ID $organizerId not found") }
+
+        existingOrganizer.name = updatedOrganizer.name
+
+        return organizerRepository.save(existingOrganizer)
+    }
+
+    override fun getOrganizer(organizerId: Long): Organizer? {
+        return organizerRepository.findById(organizerId).orElse(null)
+    }
+
+    override fun listOrganizers(): List<Organizer> {
+        return organizerRepository.findAll()
+    }
+
+    override fun deleteOrganizer(organizerId: Long) {
+        if (!organizerRepository.existsById(organizerId)) {
+            throw EntityNotFoundException("Organizer with ID $organizerId not found")
+        }
+        organizerRepository.deleteById(organizerId)
+    }
 }
