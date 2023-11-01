edfe67c0fc7d79ed61c0f58424d0a54c6c72c98b
Tawan Muadmuenwai <modhanami@gmail.com>
2023-11-01 20:31:34 +0700

    Dump POC

--- poc1/app/activity/[id]/embed/profile/page.tsx
+++ poc1/app/activity/[id]/embed/profile/page.tsx
@@ -1,35 +1,42 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../../../api/auth/[...nextauth]/route";
-import { signIn } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import Link from "next/link";
-import { getToken } from "next-auth/jwt";
+import { authOptions } from "../../../../api/auth/[...nextauth]/route";
+import { UserPoints } from "./user-points";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const session = await getServerSession(authOptions);
+  const clientSession = await getSession();
+  console.log("clientSession", clientSession);
 
   console.log("session", session);
 
-  if (!session) {
-    return (
-      <div>
-        <h1>Profile</h1>
-        <div>Not signed in</div>
-        <Link
-          href={`/api/auth/signin?callbackUrl=${encodeURIComponent(
-            `/activity/${params.id}/embed/profile`
-          )}`}
-          passHref
-        >
-          Sign in
-        </Link>
-      </div>
-    );
-  }
-
   return (
     <div>
-      <h1>Profile</h1>
+      <h1>Child: localhost:9000</h1>
       <pre>Session: {JSON.stringify(session, null, 2)}</pre>
+      <br />
+      {!session && (
+        <ul>
+          <li>
+            <Link
+              href={`/api/auth/signin?callbackUrl=${encodeURIComponent(
+                `/activity/${params.id}/embed/profile`
+              )}`}
+              passHref
+            >
+              Sign in
+            </Link>
+          </li>
+          <li>
+            <Link href={`/api/auth/print-token`} passHref>
+              Print token
+            </Link>
+          </li>
+        </ul>
+      )}
+      <br />
+      <UserPoints />
     </div>
   );
 }
