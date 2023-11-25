0a7196ca4e07ed3dc5954cddba415281620c32f7
Tawan Muadmuenwai <modhanami@gmail.com>
2023-11-25 15:40:15 +0700

    Remove H2 server bean

--- backend/src/main/kotlin/com/arsahub/backend/BackendConfiguration.kt
+++ backend/src/main/kotlin/com/arsahub/backend/BackendConfiguration.kt
@@ -1,14 +1,7 @@
 package com.arsahub.backend
 
-import org.h2.tools.Server
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
-class BackendConfiguration {
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    fun h2Server(): Server {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092")
-    }
-}
+class BackendConfiguration
