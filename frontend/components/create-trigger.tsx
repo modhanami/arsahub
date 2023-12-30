e585c8e41e162cbc86a7ecbcc247178194db1f30
Tawan Muadmuenwai <modhanami@gmail.com>
2023-12-30 11:57:03 +0700

    frontend: Add fields to trigger creation and fix auto-generated key

--- frontend/components/create-trigger.tsx
+++ frontend/components/create-trigger.tsx
@@ -18,6 +18,14 @@ import {
 } from "@/components/ui/form";
 
 import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import {
   Dialog,
   DialogClose,
   DialogContent,
@@ -25,27 +33,43 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { toast } from "./ui/use-toast";
-import { useRouter } from "next/navigation";
 import { isApiValidationError } from "@/api";
 import { Label } from "@/components/ui/label";
 import { Icons } from "@/components/icons";
 import { useCreateTrigger } from "@/hooks";
+import { DevTool } from "@hookform/devtools";
 
-export const triggerCreateSchema = z.object({
+const FieldTypeEnum = z.enum(["Text", "Integer"] as const);
+const triggerCreateSchema = z.object({
   title: z
     .string({
       required_error: "Title is required",
     })
+    .trim()
     .min(4, { message: "Must be between 4 and 200 characters" })
     .max(200, { message: "Must be between 4 and 200 characters" }),
   description: z
     .string()
     .max(500, { message: "Must not be more than 500 characters" })
     .optional(),
+  fields: z.array(
+    z.object({
+      key: z
+        .string()
+        .min(4, { message: "Must be between 4 and 200 characters" })
+        .max(200, { message: "Must be between 4 and 200 characters" })
+        .refine((value) => /[a-zA-Z0-9_-]/.test(value), {
+          message:
+            "Must contain only alphanumeric characters, underscores, and dashes",
+        }),
+      type: FieldTypeEnum,
+      label: z.string().optional(),
+    }),
+  ),
 });
 
 function generateKeyFromTitle(title: string): string | undefined {
-  const regex = /[a-zA-Z0-9_-]/g;
+  const regex = /[a-zA-Z0-9_-]+/g;
   const matches = title.match(regex);
 
   if (matches) {
@@ -60,12 +84,36 @@ function generateKeyFromTitle(title: string): string | undefined {
 type FormData = z.infer<typeof triggerCreateSchema>;
 
 export function TriggerCreateForm() {
-  const router = useRouter();
   const form = useForm<FormData>({
     resolver: zodResolver(triggerCreateSchema),
+    defaultValues: {
+      fields: [],
+    },
   });
+  console.log(form.formState.touchedFields);
   const [isOpen, setIsOpen] = React.useState(false);
   const mutation = useCreateTrigger();
+
+  function addField() {
+    const fields = form.getValues("fields");
+    console.log("fields", fields);
+    form.setValue("fields", [
+      ...fields,
+      {
+        key: "",
+        type: "Text",
+        label: "",
+      },
+    ]);
+  }
+
+  function removeField(index: number) {
+    const fields = form.getValues("fields");
+    form.setValue(
+      "fields",
+      fields.filter((_, i) => i !== index),
+    );
+  }
 
   async function onSubmit(values: FormData) {
     const generatedKey = generateKeyFromTitle(values.title);
@@ -78,7 +126,11 @@ export function TriggerCreateForm() {
         title: values.title,
         description: values.description || "",
         key: generatedKey,
-        fields: [],
+        fields: values.fields.map((field) => ({
+          key: field.key,
+          type: field.type.toLowerCase(),
+          label: field.label || null,
+        })),
       },
       {
         onSuccess: (data) => {
@@ -115,7 +167,7 @@ export function TriggerCreateForm() {
           </DialogHeader>
           {/* <CardDescription>Deploy your new activity in one-click.</CardDescription> */}
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
               {/* <Card className="w-[350px]"> */}
               {/* <CardContent> */}
               <FormField
@@ -154,21 +206,92 @@ export function TriggerCreateForm() {
                 )}
               />
 
-              <Label>Auto-generated key</Label>
-              <p className="text-gray-500 text-sm">
-                This is the key that you will use to trigger for users in this
-                activity.
-              </p>
-              <Input
-                value={
-                  form.watch("title") &&
-                  generateKeyFromTitle(form.watch("title"))
-                }
-                readOnly
-                disabled
-              />
+              <FormItem>
+                <FormLabel>Auto-generated key</FormLabel>
+                <p className="text-gray-500 text-sm">
+                  This is the key that you will use to trigger for users in this
+                  activity.
+                </p>
+                <Input
+                  value={
+                    form.watch("title") &&
+                    generateKeyFromTitle(form.watch("title"))
+                  }
+                  readOnly
+                  disabled
+                />
+              </FormItem>
 
-              <div className="flex justify-between mt-8">
+              <div className="flex items-center justify-between">
+                <Label>Fields</Label>
+                <Button variant="outline" onClick={() => addField()}>
+                  Add field
+                </Button>
+              </div>
+
+              {form.watch("fields")?.map((fieldDefinition, index) => (
+                <div key={index} className="flex gap-2 flex-auto">
+                  <FormField
+                    control={form.control}
+                    name={`fields.${index}.key`}
+                    render={({ field }) => (
+                      <FormItem className="w-3/6">
+                        <FormControl>
+                          <Input placeholder="Key" {...field} />
+                        </FormControl>
+                        {form.formState.touchedFields["fields"]?.[index]
+                          ?.key && <FormMessage />}
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name={`fields.${index}.type`}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Select
+                            onValueChange={field.onChange}
+                            value={field.value}
+                          >
+                            <SelectTrigger>
+                              <SelectValue
+                                placeholder="Select a type"
+                                className="w-1"
+                              />
+                            </SelectTrigger>
+                            <SelectContent className="w-full">
+                              {FieldTypeEnum.options.map((type) => (
+                                <SelectItem
+                                  key={type}
+                                  value={type}
+                                  className="flex items-center justify-between w-full"
+                                >
+                                  {type}
+                                </SelectItem>
+                              ))}
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <div className="w-1/6">
+                    <Button
+                      variant="ghost"
+                      onClick={() => removeField(index)}
+                      size="icon"
+                    >
+                      <Icons.trash className="h-4 w-4" />
+                    </Button>
+                  </div>
+                </div>
+              ))}
+
+              <div className="flex justify-between pt-2">
                 <DialogClose asChild>
                   <Button variant="outline" type="button">
                     Cancel
@@ -179,6 +302,8 @@ export function TriggerCreateForm() {
                 </Button>
               </div>
             </form>
+
+            <DevTool control={form.control} />
           </Form>
         </DialogContent>
       </Dialog>
