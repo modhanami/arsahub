4118491bc115769812af95ea33be68037e9c2d9d
Tawan Muadmuenwai <modhanami@gmail.com>
2024-02-15 23:19:40 +0700

    Add default app on supabase sync

--- backend/src/main/kotlin/com/arsahub/backend/services/AuthService.kt
+++ backend/src/main/kotlin/com/arsahub/backend/services/AuthService.kt
@@ -1,14 +1,20 @@
 package com.arsahub.backend.services
 
 import com.arsahub.backend.dtos.supabase.SupabaseGoogleIdentity
+import com.arsahub.backend.models.App
 import com.arsahub.backend.models.User
+import com.arsahub.backend.repositories.AppRepository
 import com.arsahub.backend.repositories.UserRepository
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class AuthService(private val userRepository: UserRepository) {
+class AuthService(
+    private val userRepository: UserRepository,
+    private val apiKeyService: APIKeyService,
+    private val appRepository: AppRepository,
+) {
     private val logger = KotlinLogging.logger {}
 
     @Transactional
@@ -21,19 +27,36 @@ class AuthService(private val userRepository: UserRepository) {
 
         return if (user == null) {
             logger.info { "User not found for $identityIdsMessage, creating user" }
-            userRepository.save(
-                User(
-                    externalUserId = identity.supabaseUserId,
-                    googleUserId = identity.googleUserId,
-                    email = identity.email,
-                    name = identity.name,
-                ),
-            )
+            val newUser =
+                userRepository.save(
+                    User(
+                        externalUserId = identity.supabaseUserId,
+                        googleUserId = identity.googleUserId,
+                        email = identity.email,
+                        name = identity.name,
+                    ),
+                )
+
+            createAppForUser(newUser) // Default app
+
+            newUser
         } else {
             logger.info { "User ${user.userId} found for $identityIdsMessage, updating user" }
             user.email = identity.email
             user.name = identity.name
             userRepository.save(user)
         }
+    }
+
+    private fun createAppForUser(user: User): App {
+        logger.info { "Creating default app for user ${user.userId}" }
+        val newApp =
+            App(
+                title = if (user.name != null) "${user.name}'s App" else "My App",
+                owner = user,
+                apiKey = apiKeyService.generateAPIKey(),
+            )
+        logger.info { "Created app ${newApp.id} for user ${user.userId}" }
+        return appRepository.save(newApp)
     }
 }
