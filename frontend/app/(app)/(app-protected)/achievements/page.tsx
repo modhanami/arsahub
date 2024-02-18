0656ee12035d6637bcc62686a245e3be45301650
Tawan Muadmuenwai <modhanami@gmail.com>
2024-02-18 12:00:41 +0700

    Fix dashboard shell layout

--- frontend/app/(app)/(app-protected)/achievements/page.tsx
+++ frontend/app/(app)/(app-protected)/achievements/page.tsx
@@ -23,5 +23,22 @@ export default function Page() {
       </DashboardHeader>
       <DataTable columns={columns} data={achievements || []} />
     </DashboardShell>
+
+    // <>
+    //   <div className="h-full flex-1 flex-col space-y-8 p-8">
+    //     <div className="flex items-center justify-between space-y-2">
+    //       <div>
+    //         <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+    //         <p className="text-muted-foreground">
+    //           Here&apos;s a list of your tasks for this month!
+    //         </p>
+    //       </div>
+    //       <div className="flex items-center space-x-2">
+    //         <UserNav />
+    //       </div>
+    //     </div>
+    //     <DataTable columns={columns} data={achievements || []} />
+    //   </div>
+    // </>
   );
 }
--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -12,8 +12,8 @@ export function DashboardHeader({
   return (
     <div className="flex items-center justify-between px-2">
       <div className="grid gap-1">
-        <h1 className="font-heading text-3xl md:text-4xl">{heading}</h1>
-        {text && <p className="text-lg text-muted-foreground">{text}</p>}
+        <h2 className="text-2xl font-bold tracking-tight">{heading}</h2>
+        {text && <p className="text-muted-foreground">{text}</p>}
       </div>
       {children}
     </div>
