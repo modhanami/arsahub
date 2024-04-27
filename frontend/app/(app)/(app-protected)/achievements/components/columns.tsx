48806cddb19d681ee34c36cb817d84453ff999c0
Tawan Muadmuenwai <modhanami@gmail.com>
2024-04-27 13:40:23 +0700

    Update achievements table

--- frontend/app/(app)/(app-protected)/achievements/components/columns.tsx
+++ frontend/app/(app)/(app-protected)/achievements/components/columns.tsx
@@ -49,24 +49,14 @@ export const columns: ColumnDef<AchievementResponse>[] = [
               radius="none"
             />
           )}
-          <span className="max-w-[400px] truncate font-medium">
-            {row.getValue("title")}
-          </span>
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: "description",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Description" />
-    ),
-    cell: ({ row }) => {
-      return (
-        <div className="flex space-x-2">
-          <span className="max-w-[400px] truncate font-medium">
-            {row.getValue("description")}
-          </span>
+          <div className="flex flex-col gap-2 max-w-[60vw]">
+            <span className="truncate font-medium">
+              {row.getValue("title")}
+            </span>
+            <span className="truncate text-muted-foreground">
+              {row.original["description"]}
+            </span>
+          </div>
         </div>
       );
     },
